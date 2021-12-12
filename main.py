4bb30d5a8be2713f192a77005ab9595ddcb58ea0
tiiedye <tiiedye@gmail.com>
2021-12-12 10:37:20 -0800

    adding the ball

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Pong
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -9,8 +11,8 @@ screen.title("Pong")
 screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
-
 l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkeypress(r_paddle.go_up, "Up")
@@ -20,7 +22,9 @@ screen.onkeypress(l_paddle.go_down, "s")
 
 game_on = True
 while game_on:
+    time.sleep(0.07)
     screen.update()
+    ball.move()
 
 
 # Exit after all code is complete
