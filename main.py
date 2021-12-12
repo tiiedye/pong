22e4582922f73ce1de565e6efe8cc3f483fabb49
tiiedye <tiiedye@gmail.com>
2021-12-12 09:30:14 -0800

    screen setup

---
+++ main.py
@@ -0,0 +1,11 @@
+# Pong
+from turtle import Screen
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen. bgcolor("black")
+screen.title("Pong")
+
+
+# Exit after all code is complete
+screen.exitonclick()
